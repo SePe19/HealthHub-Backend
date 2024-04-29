64de1f983d1b339e41fc8371a4cbaaede5b0a4ba
SePe19 <seba217i@stud.kea.dk>
2024-04-29 15:09:43 +0200

    initial commit

---
+++ src/main/java/fks/healthhub_backend/HealthHubBackendApplication.java
@@ -0,0 +1,13 @@
+package fks.healthhub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthHubBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fks/healthhub_backend/HealthHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fks.healthhub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthHubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
